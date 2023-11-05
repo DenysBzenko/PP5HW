3a01722be8e7b65513b014002831f9ea93da9f94
Denys Bzenko <dbzenko@kse.org.ua>
2023-11-05 23:00:21 +0200

    Create class CaesarCipherCLI. This is the main part of the code and the main functionality

--- main.py
+++ main.py
@@ -19,4 +19,56 @@ def decrypt(data, key):
     cipher_lib.decrypt(data.encode('utf-8'), c_int(key), buffer)
     return buffer.value.decode('utf-8')
 
+class CaesarCipherCLI:
+    def __init__(self):
+        self.data = ""
+        self.key = 0
+        self.operation = ""
+        self.result = ""
+
+    def read_data(self, method):
+        if method == "1":
+            file_name = input("Enter file name: ")
+            self.data = self.read_from_file(file_name)
+        elif method == "2":
+            self.data = input("Enter encryption/decryption: ")
+        else:
+            raise ValueError("Invalid input .")
+
+    def read_from_file(self, file_name):
+        try:
+            with open(file_name, 'r') as file:
+                return file.read()
+        except FileNotFoundError:
+            print(f"File '{file_name}' not found.")
+            return None
+
+    def write_to_file(self, file_name, data):
+        try:
+            with open(file_name, 'w') as file:
+                file.write(data)
+            print(f"Result saved to '{file_name}'.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def set_key(self):
+        self.key = int(input("Enter key (shift amount): "))
+
+    def set_operation(self):
+        self.operation = input("Select the operation encrypt or decrypt: ")
+        if self.operation not in ['encrypt', 'decrypt']:
+            raise ValueError("Invalid operation.")
+
+    def process_data(self):
+        if self.operation == 'encrypt':
+            self.result = encrypt(self.data, self.key)
+        elif self.operation == 'decrypt':
+            self.result = decrypt(self.data, self.key)
+
+    def output_result(self):
+        output_file_name = input("Enter a file name to save the result or result in console (enter): ")
+        if output_file_name:
+            self.write_to_file(output_file_name, self.result)
+        else:
+            print("Result:", self.result)
 
